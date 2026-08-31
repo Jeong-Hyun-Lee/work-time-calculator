190050ccfdcfa839063711a8ae22d6bc579d0877
Joshua Lee <joshua.lee@obigo.com>
2026-08-31 18:23:04 +0900

    style: 디자인 토큰 교체 및 Inter 웹폰트 적용

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,4 @@ import TimeCalculator from './components/TimeCalculator.vue'
 	padding: 0;
 	box-sizing: border-box;
 }
-
-body {
-	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-		Ubuntu, Cantarell, sans-serif;
-}
 </style>
